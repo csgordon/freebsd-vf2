ed5ba8be1adc68a45d28812ca403a8c16a15d174
Colin S. Gordon <colin.s.gordon@gmail.com>
2025-06-09 12:59:51 -0400

    Update mkvf2img.sh to use 15-CURRENT and Sophgo DTBs

--- mkvf2img.sh
+++ mkvf2img.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-BASE_URL=https://download.freebsd.org/snapshots/riscv/riscv64/ISO-IMAGES/14.0
-BASE_IMAGE=FreeBSD-14.0-ALPHA2-riscv-riscv64-GENERICSD-20230818-77013f29d048-264841.img
-#BASE_IMAGE=FreeBSD-14.0-CURRENT-riscv-riscv64-GENERICSD-20230420-f369f10dd812-262341.img
+BASE_URL=https://download.freebsd.org/snapshots/riscv/riscv64/ISO-IMAGES/15.0
+BASE_IMAGE=FreeBSD-15.0-CURRENT-riscv-riscv64-20250606-fa02d9fceab7-277727-memstick.img
 
 DTB_FILE=jh7110-starfive-visionfive-2-v1.3b.dtb
 
@@ -24,10 +23,13 @@ fi
 mdconfig -a -t vnode -f $BASE_IMAGE -u 0
 
 # extract the esp and root partitions into their own files
+# Recent 15.0 snapshots don't label the partitions as efi and rootfs
 echo 'Extracting EFI partition image...'
-dd if=$BASE_IMAGE of=efi.img bs=512 $(gpart show -l /dev/md0 | grep efi | awk '{ printf "skip=%d count=%d", $1, $2 }')
+#dd if=$BASE_IMAGE of=efi.img bs=512 $(gpart show -l /dev/md0 | grep efi | awk '{ printf "skip=%d count=%d", $1, $2 }')
+dd if=$BASE_IMAGE of=efi.img bs=512 $(gpart show -l /dev/md0 | head -2 | tail -1 | awk '{ printf "skip=%d count=%d", $1, $2 }')
 echo 'Extracting root partition image...'
-dd if=$BASE_IMAGE of=root.img bs=512 $(gpart show -l /dev/md0 | grep rootfs | awk '{ printf "skip=%d count=%d", $1, $2 }')
+#dd if=$BASE_IMAGE of=root.img bs=512 $(gpart show -l /dev/md0 | grep rootfs | awk '{ printf "skip=%d count=%d", $1, $2 }')
+dd if=$BASE_IMAGE of=root.img bs=512 $(gpart show -l /dev/md0 | head -3 | tail -1 | awk '{ printf "skip=%d count=%d", $1, $2 }')
 
 # unmount it
 mdconfig -d -u 0
@@ -36,9 +38,11 @@ mdconfig -d -u 0
 # mount the esp image to put the dtb in
 echo 'Adding VisionFive2 dtb to EFI image...'
 mdconfig -a -t vnode -f efi.img -u 0
-mount_msdosfs /dev/md0 /mnt
-mkdir -p /mnt/dtb/starfive
-cp -v $DTB_FILE /mnt/dtb/starfive/jh7110-starfive-visionfive-2-v1.3b.dtb
+mount_msdosfs /dev/md0 localmnt
+mkdir -p localmnt/dtb/starfive
+cp -v $DTB_FILE localmnt/dtb/starfive/jh7110-starfive-visionfive-2-v1.3b.dtb
+mkdir -p localmnt/dtb/milkv
+cp -v cv181x*.dtb localmnt/dtb/milkv/
 umount /dev/md0
 mdconfig -d -u 0
 
@@ -48,26 +52,26 @@ echo 'Customizing root image...'
 #truncate -s +2G root.img
 mdconfig -a -t vnode -f root.img -u 0
 #growfs -y /dev/md0
-mount /dev/md0 /mnt
-#rsync -av /riscv/ /mnt
+mount /dev/md0 localmnt
+#rsync -av /riscv/ localmnt
 
 # make updates
-echo 'root_rw_mount="NO"' >> /mnt/etc/rc.conf
-echo 'mmc_load="YES"' >> /mnt/boot/loader.conf
-echo 'mmcsd_load="YES"' >> /mnt/boot/loader.conf
-echo 'sdhci_load="YES"' >> /mnt/boot/loader.conf
-echo 'sdio_load="YES"' >> /mnt/boot/loader.conf
-rm -f /mnt/etc/fstab
-touch /mnt/etc/fstab
+echo 'root_rw_mount="NO"' >> localmnt/etc/rc.conf
+echo 'mmc_load="YES"' >> localmnt/boot/loader.conf
+echo 'mmcsd_load="YES"' >> localmnt/boot/loader.conf
+echo 'sdhci_load="YES"' >> localmnt/boot/loader.conf
+echo 'sdio_load="YES"' >> localmnt/boot/loader.conf
+rm -f localmnt/etc/fstab
+touch localmnt/etc/fstab
 
 echo 'Pausing for extra copyover:'
-echo "New kernel & modules can be patched with sudo make CROSS_TOOLCHAIN=... TARGET_ARCH=risv64 DESTDIR=/mnt reinstallkernel (if you've already done buildkernel)"
+echo "New kernel & modules can be patched with sudo make CROSS_TOOLCHAIN=... TARGET_ARCH=risv64 DESTDIR=localmnt reinstallkernel (if you've already done buildkernel)"
 echo "If this is done, or you're not patching, press enter to continue"
 read ans
 echo 'Continuing...'
 
 # and unmount
-umount /mnt
+umount localmnt
 mdconfig -d -u 0
 
 
@@ -77,7 +81,7 @@ mkuzip -A zstd -d -o root.img.uzip root.img
 
 
 # source and target mountpoints
-mkdir -p /mnt/s /mnt/t
+mkdir -p localmnt/s localmnt/t
 
 
 # create empty boot image
@@ -88,17 +92,17 @@ newfs -L bootfs /dev/md1
 
 # mount root and boot
 mdconfig -a -t vnode -f root.img -u 0
-mount -o ro /dev/md0 /mnt/s
-mount /dev/md1 /mnt/t
+mount -o ro /dev/md0 localmnt/s
+mount /dev/md1 localmnt/t
 
 # and copy over everything we need
 echo 'Copying over files for the bootloader...'
-cp -rv /mnt/s/boot/ /mnt/t/boot
-cp -v root.img.uzip /mnt/t
+cp -rv localmnt/s/boot/ localmnt/t/boot
+cp -v root.img.uzip localmnt/t
 
 # unmount it all
-umount /mnt/s
-umount /mnt/t
+umount localmnt/s
+umount localmnt/t
 mdconfig -d -u 0
 mdconfig -d -u 1
 
